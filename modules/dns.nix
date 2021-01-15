afc289003e03b46444692b90e0b8898ac7205b59
Brian Olsen <brian@maven-group.org>
2021-01-15 17:52:53 +0100

    Add unbound control setup

--- modules/dns.nix
+++ modules/dns.nix
@@ -2,6 +2,23 @@
 with lib;
 let
   cfg = config.fefa;
+  ucfg = config.services.unbound;
+  stateDir = "/var/lib/unbound";
+  unboundWrapped = pkgs.stdenv.mkDerivation {
+      name = "unbound-wrapped";
+
+      buildInputs = [ pkgs.makeWrapper ucfg.package ];
+
+      phases = [ "installPhase" ];
+
+      installPhase = ''
+        mkdir -p "$out/bin"
+        makeWrapper ${ucfg.package}/bin/unbound-control $out/bin/unbound-control \
+          --add-flags "-c ${stateDir}/unbound.conf"
+        makeWrapper ${ucfg.package}/bin/unbound-checkconf $out/bin/unbound-checkconf \
+          --add-flags "${stateDir}/unbound.conf"
+      '';
+    };
 in
 {
   config = mkIf (cfg.enable && (cfg.localDnsResolver || (cfg.dnsForwarder != ""))) {
@@ -19,6 +36,7 @@ in
       '';
     };
     */
+    environment.systemPackages = mkIf cfg.localDnsResolver [ (hiPrio unboundWrapped) ];
     services.unbound = mkIf cfg.localDnsResolver {
       enable = true;
       forwardAddresses = mkIf (cfg.dnsForwarder != "") [cfg.dnsForwarder];
@@ -26,9 +44,25 @@ in
       #  server:
       #    val-permissive-mode: yes
       #'';
+      extraConfig = ''
+        remote-control:
+          control-enable: "yes"
+          server-key-file: ${stateDir}/unbound_server.key
+          server-cert-file: ${stateDir}/unbound_server.pem
+          control-key-file: ${stateDir}/unbound_control.key
+          control-cert-file: ${stateDir}/unbound_control.pem
+          control-port: 8953
+          control-interface: 127.0.0.1
+      '';
     };
     networking.nameservers = if cfg.localDnsResolver
       then ["127.0.0.1"]
       else [cfg.dnsForwarder];
+    systemd.services.unbound = mkIf cfg.localDnsResolver {
+      path = [ pkgs.openssl ];
+      preStart = ''
+        ${ucfg.package}/bin/unbound-control-setup -d ${stateDir}
+      '';
+    };
   };
 }
